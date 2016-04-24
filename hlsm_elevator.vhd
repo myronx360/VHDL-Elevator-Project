ff7f97d6dcd951d2e9abfc7915000b8b18cc40dd
myronx360 <mxw13@hotmail..com>
2016-04-24 19:56:06 -0400

    added queue
    not yet working

--- hlsm_elevator.vhd
+++ hlsm_elevator.vhd
@@ -1,13 +1,19 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
-use IEEE.numeric_std.ALL;
+use IEEE.std_logic_arith.all;
+use IEEE.std_logic_unsigned.all;
 
 
 entity hlsm_elevator is
 
-port (clk, DrC: inout bit; rst: in bit; T, B: inout bit; Fo, Fc, MS, TMR: in bit; Up, Dn, Fl : in bit_vector (2 downto 0); LE, Q: inout bit_vector (2 downto 0); Fstart, Fstop: in bit);
+port (	clk, DrC: inout bit; 
+	rst: in bit; T, B: inout bit; 
+	Fo, Fc, MS : in bit; TMR : inout bit; 
+	Up, Dn, Fl : in bit_vector (2 downto 0); 
+	LE, Q: inout bit_vector (2 downto 0); 
+	Fstart, Fstop: inout bit;
+	Elv1, Elv2 : inout bit_vector (2 downto 0);
+	ElvSelected : inout bit);
 end hlsm_elevator;
 
 architecture beh of hlsm_elevator is 
@@ -29,13 +35,12 @@ begin
 		 currentstate <= nextstate;
 		end if;
 	end process statereg;
-
+		
 	comblogic: process(currentstate, T, B, Fo, Fc, DrC, MS, Up, Dn, Fl, TMR, Fstart, Fstop)
 	
 	begin
 
 	case currentstate is
-
   	-- init
 	when init =>
 	Q <= "100";
@@ -43,6 +48,8 @@ begin
 	T <= '0';
 	B <= '0';
 	DrC <= '0';
+	Elv1 <= "000";
+	Elv2 <= "000";
 	nextstate <= waitState;
 
 	-- wait
@@ -54,17 +61,18 @@ begin
 		Q <= Up;
 	elsif (Dn'event and Dn /= "100") then
 		Q <= Dn;
-	else
-		--Q <= "100";
 	end if;
-
-
-  	if (Q = LE) then
+	
+  	if (Q = Elv1) then
+		ElvSelected <= '0';
 		nextstate <= openState;
-	elsif (Q /= LE) and (Q /= "100") then
+	elsif (Q = Elv2) then
+		ElvSelected <= '1';
+		nextstate <= openState;
+	elsif (Q /= Elv1 or Q/= Elv2) and (Q /= "100") then
 		nextstate <= Acc;
   	elsif (MS = '1') then
-		nextstate <= waitState;
+		nextstate <= openState;
   	end if;
 	
   	-- open
@@ -73,17 +81,19 @@ begin
 	T <= '0';
 	B <= '0';
 	DrC <= '1';
+	TMR <= '1' after 25 ns;
 	
 	--Fstop <= '0';
 	--Fstart <= '0';
-  	if (Fc = '1') then
-  		nextstate <= close;
-  	elsif (MS = '1' or Fo = '1') then
+  	if (MS = '1' or Fo = '1') then
   		nextstate <= openState;
+  	elsif (TMR = '1' or Fc = '1') then
+  		nextstate <= close;
 	end if;
 	
   	--close
 	when close => 
+	TMR <= '0';
 	DrC <= '0';
   	if (Q = "100" or Q = "101" or Q = "110" or Q = "111") then
 		nextstate <= waitState;
@@ -98,36 +108,57 @@ begin
 		else
 			nextstate <= Const;
 	 	end if;
-
   	--const
 	when Const => 
-
-		if (Q > LE) then -- go Up
-			if(LE = "000") then LE <= "001";
+	if (ElvSelected = '0') then
+		if (Q > Elv1) then -- go Up
+			if(Elv1 = "000") then Elv1 <= "001";
 				if(Q = "001") then T <= '1'; end if;			
-			elsif(LE = "001") then LE <= "010";
+			elsif(Elv1 = "001") then Elv1 <= "010";
 				if(Q = "010") then T <= '1'; end if;
-			elsif(LE = "010") then LE <= "011";
+			elsif(Elv1 = "010") then Elv1 <= "011";
 				if(Q = "011") then T <= '1'; end if;
 			end if;
-		elsif (Q < LE) then  --go Dn
-			if(LE = "011") then LE <= "010";
+		elsif (Q < Elv1) then  --go Dn
+			if(Elv1 = "011") then Elv1 <= "010";
 				if(Q = "010") then B <= '1'; end if;
-			elsif(LE = "010") then LE <= "001";
+			elsif(Elv1 = "010") then Elv1 <= "001";
 				if(Q = "001") then B <= '1'; end if;
-			elsif(LE = "001") then LE <= "000";
+			elsif(Elv1 = "001") then Elv1 <= "000";
 				if(Q = "000") then B <= '1'; end if;
 			end if;
 		end if;
-
+			if ((T = '1' or B = '1')) and (Elv1 = Q) then
+			nextstate <= Dec;
+			else
+  			nextstate <= Const;
+ 			end if;
+	end if;
 	
-	
-
-	if ((T = '1' or B = '1')) and (LE = Q) then
-		nextstate <= Dec;
-	else
-  		nextstate <= Const;
- 	end if;
+	if (ElvSelected = '1') then
+		if (Q > Elv2) then -- go Up
+			if(Elv2 = "000") then Elv2 <= "001";
+				if(Q = "001") then T <= '1'; end if;			
+			elsif(Elv2 = "001") then Elv2 <= "010";
+				if(Q = "010") then T <= '1'; end if;
+			elsif(Elv2 = "010") then Elv2 <= "011";
+				if(Q = "011") then T <= '1'; end if;
+			end if;
+		elsif (Q < Elv2) then  --go Dn
+			if(Elv2 = "011") then Elv2 <= "010";
+				if(Q = "010") then B <= '1'; end if;
+			elsif(Elv2 = "010") then Elv2 <= "001";
+				if(Q = "001") then B <= '1'; end if;
+			elsif(Elv2 = "001") then Elv2 <= "000";
+				if(Q = "000") then B <= '1'; end if;
+			end if;
+		end if;
+		if ((T = '1' or B = '1')) and (Elv1 = Q) then
+			nextstate <= Dec;
+		else
+  			nextstate <= Const;
+ 		end if;
+	end if;
   
 	--Dec
 	when Dec => 
@@ -150,8 +181,8 @@ begin
 
 
 	--tb
-library ieee;
-use ieee.std_logic_1164.all;
+--ibrary ieee;
+--use ieee.std_logic_1164.all;
 use work.all;
 
 entity hlsm_elevator_tb is
@@ -162,8 +193,18 @@ architecture beh of hlsm_elevator_tb is
 
 component c1
 
-port (clk, DrC : inout bit; rst: in bit; T, B : inout bit; Fo, Fc, MS, TMR: in bit; Up, Dn, Fl: in bit_vector (2 downto 0); LE: inout bit_vector (2 downto 0);Fstart, Fstop: in bit);
+port (clk, DrC : inout bit; rst: in bit; T, B : inout bit; Fo, Fc, MS : in bit; TMR: inout bit; Up, Dn, Fl: in bit_vector (2 downto 0); LE: inout bit_vector (2 downto 0); Fstart, Fstop: inout bit);
 
+end component;
+
+component queue
+port(
+         clk : in bit;
+         push : in bit;
+         pull : in bit;
+         din : in bit_vector (2 downto 0);
+         dout : out bit_vector(2 downto 0)
+         );
 end component;
 
 signal ct : bit;
@@ -180,43 +221,73 @@ signal Upt: bit_vector (2 downto 0);
 signal Flt: bit_vector (2 downto 0);
 signal Dnt: bit_vector (2 downto 0);
 signal Fstartt, Fstopt: bit;
-
+--signal Trigger : bit;
+------------queue signals----------------------
+signal pusht, pullt: bit; 
+signal dint,doutt: bit_vector(2 downto 0);
 
 for all: c1 use entity work.hlsm_elevator(beh);
+for all: queue use entity work.queue_8nibble(queue_8nibble_arc);
 
 begin
 g1: c1 port map(ct, DrCt, rt, Tt, Bt, Fot, Fct, MSt, TMRt, Upt, Dnt, Flt, LEt, Fstartt, Fstopt);
-	--------------------Basic Elevator----------------------------------------------
---	rt <= '0', '1' after 5000 ns;
---	Fot <= '0', '1' after 4500 ns, '0' after 4750 ns;
---	Fct <= '0', '1' after 4700 ns;
---	Fstartt <= '0';
---	Fstopt <= '0';
---	MSt <= '0';
---	TMRt <= '0', '1' after 100 ns, '0' after 125 ns,'1' after 150 ns, '0' after 250 ns, '1' after 300 ns, '0' after 350 ns, '1' after 400 ns, '0' after 415 ns,'0' after 550 ns,'1' after 600 ns,'0' after 650 ns,'1' after 700 ns;
---	Upt <= "100", "011" after 60 ns, "100" after 80 ns,"010" after 530 ns;
---	Flt <= "100", "010" after 225 ns, "100" after 245 ns;
---	Dnt <= "100", "000" after 325 ns, "100" after 345 ns;
+	
+	rt <= '0', '1' after 5000 ns;
+	Fot <= '0', '1' after 4500 ns, '0' after 4750 ns;
+	Fct <= '0', '1' after 4700 ns;
+	--Fstartt <= '0';
+	--Fstopt <= '0';
+	MSt <= '0';
+	--TMRt <= '0', '1' after 100ns, '0' after 125ns,'1' after 150ns, '0' after 250ns, '1' after 300ns, '0' after 350ns, '1' after 400ns, '0' after 415ns,'0' after 550ns,'1' after 600ns,'0' after 650ns,'1' after 700ns;
+	Upt <= "100", "011" after 60 ns, "100" after 80 ns,"010" after 530 ns;
+	Flt <= "100", "010" after 225 ns, "100" after 245 ns;
+	Dnt <= "100", "000" after 325 ns, "100" after 345 ns;
 
------------------------------- Force open and close Test--------------------------------------------
-	Fot <= '0', '1' after 4500 ns, '0' after 4750 ns;
-	Fct <= '0', '1' after 4700 ns;
-	Fstartt <= '0';
-	Fstopt <= '0';
-	MSt <= '0';
-	TMRt <= '0', '1' after 100 ns, '0' after 125 ns,'1' after 150 ns, '0' after 250 ns, '1' after 300 ns, '0' after 350 ns, '1' after 400 ns, '0' after 415 ns,'0' after 550 ns,'1' after 600 ns,'0' after 650 ns,'1' after 700 ns;
-	Upt <= "100", "011" after 60 ns, "100" after 80 ns,"010" after 530 ns;
-	Flt <= "100", "010" after 225 ns, "100" after 245 ns;
-	Dnt <= "100", "000" after 325 ns, "100" after 345 ns;
------------------------------- Force Start and Stop Test--------------------------------------------
-	Fot <= '0', '1' after 4500 ns, '0' after 4750 ns;
-	Fct <= '0', '1' after 4700 ns;
-	Fstartt <= '0';
-	Fstopt <= '0';
-	MSt <= '0';
-	TMRt <= '0', '1' after 100 ns, '0' after 125 ns,'1' after 150 ns, '0' after 250 ns, '1' after 300 ns, '0' after 350 ns, '1' after 400 ns, '0' after 415 ns,'0' after 550 ns,'1' after 600 ns,'0' after 650 ns,'1' after 700 ns;
-	Upt <= "100", "011" after 60 ns, "100" after 80 ns,"010" after 530 ns;
-	Flt <= "100", "010" after 225 ns, "100" after 245 ns;
-	Dnt <= "100", "000" after 325 ns, "100" after 345 ns;
+	--Trigger <=  '0', '1' after 200ns, '0' after 250ns, '1' after 300ns, '0' after 350ns, '1' after 400ns, '0' after 450ns,'1' after 500ns;
+	
+qu: queue port map (ct, pusht, pullt,dint,doutt);
 
 end beh;
+
+use work.all;
+entity queue_8nibble is
+     port(
+         clk : in bit;
+         push : in bit;
+         pull : in bit;
+         din : in bit_vector (2 downto 0);
+         dout : out bit_vector(2 downto 0)
+         );
+end queue_8nibble;
+
+
+architecture queue_8nibble_arc of queue_8nibble is
+
+type mem is array (0 to 5) of bit_vector (2 downto 0);
+
+signal queue : mem := (others=>(others=>'0'));
+
+begin
+
+    queue_design : process (clk,push,pull,din) is   
+    variable mem : bit_vector (2 downto 0) ;
+    variable i : integer := 0;
+    begin                   
+        if (rising_edge (clk)) then
+            if (push='1') then
+                queue(i) <= din;  
+                if (i<5) then
+                    i := i + 1;       
+                end if;
+            elsif (pull='1') then   
+                dout <= queue(0); 
+                if (i>0) then
+                    i := i - 1;
+                end if;
+                queue(0 to 4) <= queue(1 to 5);
+            end if;
+        end if;
+    end process queue_design;
+   
+
+end queue_8nibble_arc;
